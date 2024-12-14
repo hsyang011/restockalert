1ab1b0ccbd8a451f8690904d32e46b34d9138530
hsyang011 <hsyang011@gmail.com>
2024-12-14 09:32:54 +0900

    init commit

---
+++ src/main/java/me/yangsongi/restockalert/RestockalertApplication.java
@@ -0,0 +1,13 @@
+package me.yangsongi.restockalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestockalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestockalertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/yangsongi/restockalert/RestockalertApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yangsongi.restockalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestockalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
